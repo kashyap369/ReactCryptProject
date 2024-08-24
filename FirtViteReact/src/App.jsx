31bbdb918a2e943bb0cbc02bd37807dc4871fa7e
kashyap369 <shubham983346@gmai.com>
2024-08-25 00:47:33 +0530

    last upde

--- FirtViteReact/src/App.jsx
+++ FirtViteReact/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>hello shubham its been boring but worth it</h1>
+      <h1 className="w-full h-screen bg-zinc-500">hello shubham its been boring but worth it</h1>
     </div>
   );
 }
